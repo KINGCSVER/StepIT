13b1b16b9742d098c278483ee9a501eff5b0661a
Cavad Rahimli <cavad.rehimlicl@gmail.com>
2023-08-23 15:17:22 +0400

    Branch C# created

--- Homework/18.06.2023/Transport/Transport/Car.h
+++
@@ -1,9 +0,0 @@
-#include "TransportData.h"
-
-class Car : public TransportData {
-public:
-    double calculatePassengerTime(int passengers) override;
-    double calculateCargoTime(int cargo) override;
-    double calculatePassengerCost(int passengers) override;
-    double calculateCargoCost(int cargo) override;
-};
--- Homework/30.05.2023/Student/30.05.2023 Student/30.05.2023 Student.cpp
+++
@@ -1,8 +0,0 @@
-#include "Classroom.h"
-
-int main()
-{
-	cout << "Sorry for empty main :(";
-
-	return 0;
-}
