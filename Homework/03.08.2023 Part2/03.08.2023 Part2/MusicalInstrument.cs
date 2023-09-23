dc12208d873d234b3c001a0c3c274af465a9ebd2
Cavad Rahimli <cavad.rehimlicl@gmail.com>
2023-09-24 02:36:55 +0400

    Homework 03.09.2023 Part2 added

---
+++ Homework/03.08.2023 Part2/03.08.2023 Part2/MusicalInstrument.cs
@@ -0,0 +1,53 @@
+﻿/*class MusicalInstrument
+{
+    private string name;
+    private string description;
+    private string history;
+
+    public MusicalInstrument(string name, string description, string _history)
+    {
+        this.name = name;
+        this.description = description;
+        this.history = _history;
+    }
+
+    public void Sound()
+    {
+        Console.WriteLine(name + " emits the sound of a musical instrument");
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Name of the musical instrument: " + name);
+    }
+
+    public void Desc()
+    {
+        Console.WriteLine("Description of a musical instrument: \n" + description);
+    }
+
+    public void History()
+    {
+        Console.WriteLine("The history of the creation of a musical instrument: \n" + history);
+    }
+}
+
+class Violin : MusicalInstrument
+{
+    public Violin(string name, string description, string _history) : base(name, description, _history) { }
+}
+
+class Trombone : MusicalInstrument
+{
+    public Trombone(string name, string description, string _history) : base(name, description, _history) { }
+}
+
+class Ukulele : MusicalInstrument
+{
+    public Ukulele(string name, string description, string _history) : base(name, description, _history) { }
+}
+
+class Cello : MusicalInstrument
+{
+    public Cello(string name, string description, string _history) : base(name, description, _history) { }
+}*/
