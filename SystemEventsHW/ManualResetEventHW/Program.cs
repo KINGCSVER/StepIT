8aa0996a1b48c54fb0288c7691832cd53a213be5
Javad <cavad.rehimlicl@gmail.com>
2024-02-27 17:45:24 +0400

    System Events Homework added

---
+++ SystemEventsHW/ManualResetEventHW/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Start of main");
+
+        using ManualResetEvent manualResetEvent = new(false);
+
+        var semaphore = new Semaphore(1, 5);
+
+        for (int i = 0; i < 5; i++)
+        {
+            ThreadPool.QueueUserWorkItem(
+            (p) =>
+            {
+                var res = semaphore.WaitOne();
+
+                Console.WriteLine($"Is ThreadPool Thread {Thread.CurrentThread.IsThreadPoolThread}");
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+                semaphore.Release();
+                manualResetEvent.Set();
+            });
+        }
+
+        Thread.Sleep(100);
+
+        WaitHandle.WaitAll(new WaitHandle[]{ manualResetEvent});
+
+        Console.WriteLine("End of main");
+    }
+}
